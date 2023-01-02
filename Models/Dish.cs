5ade8c70d58fa012ebe3fa4581de45dc5cd62638
zp9421@student.uni-lj.si <picman.zan@gmail.com>
2023-01-02 16:27:08 +0100

    azure fix

--- Models/Dish.cs
+++ Models/Dish.cs
@@ -6,7 +6,7 @@
         public int RestaurantID { get; set; }
         public string Name { get; set; }
         public string? ImageURL { get; set; }
-        public virtual Restaurant? Restaurant { get; set; }
+         public virtual Restaurant? Restaurant { get; set; }
         public virtual ICollection<DishRating>? DishRatings { get; set; }
 
         public string getAverageRating()

3b01c6418d0425f24602402a32b8b2dc54f82afe
zp9421@student.uni-lj.si <picman.zan@gmail.com>
2023-01-08 18:34:53 +0100

    API

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddDefaultIdentity<LunchAdvisorUser>(options => options.SignIn.
                 .AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews()
